3aeb5759af35769265d106846e024da16fba37b3
Maksim Belov <ultramaksim@gmail.com>
2025-01-06 21:42:22 +0300

    Update[turnip]: update the driver and loader required API level

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -173,7 +173,7 @@ static void set_vulkan_ptr(void* ptr) {
 
 void load_vulkan() {
     if(getenv("POJAV_ZINK_PREFER_SYSTEM_DRIVER") == NULL &&
-        android_get_device_api_level() >= 28) { // the loader does not support below that
+        android_get_device_api_level() >= 29) { // the loader does not support below API 28, turnip requires 29
 #ifdef ADRENO_POSSIBLE
         void* result = load_turnip_vulkan();
         if(result != NULL) {

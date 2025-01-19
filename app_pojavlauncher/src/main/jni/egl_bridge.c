269c2b3cb451fddcd3d6dd1aeb93818af3bbf2d3
artdeell <ultramaksim@gmail.com>
2025-01-19 23:13:00 +0300

    Fix[egl_bridge]: remove attachEnvs()

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -214,16 +214,12 @@ int pojavInitOpenGL() {
 
 extern void updateMonitorSize(int width, int height);
 
-static void attachEnvs() {
+EXTERNAL_API int pojavInit() {
     pojav_environ->glfwThreadVmEnv = get_attached_env(pojav_environ->runtimeJavaVMPtr);
     if(pojav_environ->glfwThreadVmEnv == NULL) {
         printf("Failed to attach Java-side JNIEnv to GLFW thread\n");
         return 0;
     }
-}
-
-EXTERNAL_API int pojavInit() {
-
     ANativeWindow_acquire(pojav_environ->pojavWindow);
     pojav_environ->savedWidth = ANativeWindow_getWidth(pojav_environ->pojavWindow);
     pojav_environ->savedHeight = ANativeWindow_getHeight(pojav_environ->pojavWindow);
